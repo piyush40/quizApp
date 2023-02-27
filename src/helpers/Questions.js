8e8a699f74483c718b3ff5c7770566e9496dc40a
Piyush Kumar Singh <piyush.pks456@gmail.com>
2023-02-27 21:10:11 +0530

    initial commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "sum of the 5 + 7?",
+      optionA: "15",
+      optionB: "18",
+      optionC: "16",
+      optionD: "12",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "none ",
+      optionD: "Java",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+  ];
